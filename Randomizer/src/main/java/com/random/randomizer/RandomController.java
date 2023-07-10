ce4005449d5de5bb2efd9a5d578f6ca0be2f5e62
Sam Campbell <samcam@umich.edu>
2023-07-09 23:54:49 -0400

    Tabs

--- Randomizer/src/main/java/com/random/randomizer/RandomController.java
+++ Randomizer/src/main/java/com/random/randomizer/RandomController.java
@@ -1,14 +1,39 @@
 package com.random.randomizer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-public class RandomController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RandomController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> miscOne;
+
+    @FXML
+    private ChoiceBox<String> miscTwo;
+
+    @FXML
+    private ChoiceBox<String> miscThree;
+
+    @FXML
+    private ChoiceBox<String> miscFour;
+
+    private String[] misc = {"None", "Environmental Effects", "Free City Modifiers", "Rule Modifiers", "Unit Modifiers"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        miscOne.getItems().addAll(misc);
+        miscTwo.getItems().addAll(misc);
+        miscThree.getItems().addAll(misc);
+        miscFour.getItems().addAll(misc);
+
+        miscOne.setValue("None");
+        miscTwo.setValue("None");
+        miscThree.setValue("None");
+        miscFour.setValue("None");
     }
 }
